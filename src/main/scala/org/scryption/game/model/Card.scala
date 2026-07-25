b9a81c2158566395258822362f2eb19ca586012f
Icobart <enrico.cornacchia@studio.unibo.it>
2026-07-25 19:36:59 +0200

    removed old code

--- src/main/scala/org/scryption/game/model/Card.scala
+++ src/main/scala/org/scryption/game/model/Card.scala
@@ -2,48 +2,6 @@ package org.scryption.game.model
 
 import org.scryption.game.model.Rarity.Common
 
-/*import org.scryption.game.model.SacrificeAttribute.Blood
-
-case class Card(name: String, attack: Int, life: Int, sacrificeAttribute: SacrificeAttribute, seals : Set[Seal])
-
-
-object Card:
-
-
-  def card: Card =  Card("", 0, 0, SacrificeAttribute.Nil(), Set())
-
-  extension (card: Card)
-
-    infix def Named(name: String): Card =
-      card.copy(name = name)
-
-    infix def WithAttack(attack: Int): Card = {
-      assert(attack >= 0)
-      card.copy(attack = attack)
-    }
-
-    infix def WithLife(life: Int): Card =
-      card.copy(life = life)
-
-    infix def WithSacrifice(sacrificeAttribute: SacrificeAttribute): Card =
-      card.copy(sacrificeAttribute = sacrificeAttribute)
-
-    infix def AddSeal(seal: Seal): Card =
-      card.copy(seals = card.seals + seal)
-
-  @main def main: Unit =
-    import Card.*
-
-
-    val card3 = card AddSeal Seal.Sprinter
-    val card4 = card Named "Test Card 4" WithAttack -10 WithLife 10 WithSacrifice Blood(10)
-
-
-    println(card3)
-    println(card4)
-
- */
-
 sealed trait Card:
   def name: String
   def health: Int
