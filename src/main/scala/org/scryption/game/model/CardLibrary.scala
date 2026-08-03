852f782c7a00e3a570162d6d06347fdb0a83ad58
Icobart <enrico.cornacchia@studio.unibo.it>
2026-08-03 15:56:32 +0200

    added method withRarity to Card and implemented generateRandomCards utility method in CardLIbrary with tests

--- src/main/scala/org/scryption/game/model/CardLibrary.scala
+++ src/main/scala/org/scryption/game/model/CardLibrary.scala
@@ -3,6 +3,8 @@ package org.scryption.game.model
 import org.scryption.game.model.SacrificeAttribute.{Blood, Bones}
 import org.scryption.game.model.Seal.*
 
+import scala.util.Random
+
 object CardLibrary:
 
   def squirrel: Card[?] = CreatureCard.empty named "Squirrel" withAttack 0 withHealth 1
@@ -28,4 +30,31 @@ object CardLibrary:
   def boulder: Card[?] =
     SupportCard.empty named "Boulder" withHealth 5 addSeal Wall
 
+  def mantisGod: Card[?] =
+    CreatureCard.empty named "Mantis God" withAttack 1 withHealth 1 withSacrificeAttribute Blood(1) addSeal TrifurcatedStrike withRarity Rarity.Rare
+
+  def urayuli: Card[?] =
+    CreatureCard.empty named "Urayuli" withAttack 7 withHealth 7 withSacrificeAttribute Blood(4) withRarity Rarity.Rare
+
   def getADeckWithAllTheLibrary: Deck.Deck = Deck.fromList(List(squirrel, bear))
+
+  private val commonCards: List[() => Card[?]] = List(
+    () => squirrel, () => bear, () => stoat, () => wolf, () => sparrow, () => adder, () => mantis, () => opossum
+  )
+
+  private val rareCards: List[() => Card[?]] = List(
+    () => mantisGod, () => urayuli
+  )
+
+  /** Generates N random cards based on the rarity requested
+   * @param rarity The rarity required.
+   * @param n The number of cards required to be generated.
+   * @param seed Optional seed for reproducible shuffles.
+   * @return a list with the generated cards*/
+  def generateRandomCards(rarity: Rarity, n: Int, seed: Long = Random.nextLong()): List[Card[?]] =
+    val pool = rarity match
+      case Rarity.Common => commonCards
+      case Rarity.Rare => rareCards
+    val random = new Random(seed)
+    random.shuffle(pool).take(n).map(factory => factory())
+
