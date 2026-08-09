95fd406b6120ecd4ddad5af2630fd7e54c6d758f
Icobart <enrico.cornacchia@studio.unibo.it>
2026-08-09 10:54:36 +0200

    resolveSacrificies now doesn't remove cards with the ManyLives seal, tested

--- src/main/scala/org/scryption/game/model/SacrificeManager.scala
+++ src/main/scala/org/scryption/game/model/SacrificeManager.scala
@@ -4,7 +4,8 @@ import org.scryption.game.model.boardModel.Board
 
 object SacrificeManager:
 
-  /** Manages the sacrifices on the board by clearing the appropriate slots.
+  /** Manages the sacrifices on the board by clearing the appropriate slots,
+   * taking into account the ManyLives seal.
    *
    * @param board The current state of the board.
    * @param sacrificedSlots A list of (row, col) coordinates representing the cards for sacrifice.
@@ -13,6 +14,8 @@ object SacrificeManager:
     sacrificedSlots.foldLeft(board): (currentBoard, coordinates) =>
       val (row, col) = coordinates
       currentBoard(row)(col) match
-        case Some(card) =>
+        case Some(card) if card.seals.contains(Seal.ManyLives) =>
+          currentBoard
+        case Some(_) =>
           currentBoard.updatedSlot(row, col, boardModel.x)
         case None => currentBoard
--- src/test/scala/org/scryption/game/model/SacrificeManagerTest.scala
+++ src/test/scala/org/scryption/game/model/SacrificeManagerTest.scala
@@ -9,12 +9,13 @@ import org.scryption.game.model.boardModel.{BoardRow, x, |, Board}
 class SacrificeManagerTest extends AnyFeatureSpec with GivenWhenThen with Matchers with ScalaCheckPropertyChecks:
 
   private val squirrel = CardLibrary.squirrel
+  private val cat = CardLibrary.cat
 
   Feature("Sacrifice resolution on the board"):
     Scenario("Sacrificing a normal card removes it from the board"):
       Given("A board with a normal squirrel at row 0, column 0")
-      val row0: BoardRow = Some(squirrel) | x | x | x
-      val board: Board = Board(row0, BoardRow.empty, BoardRow.empty)
+      val row0 = Some(squirrel) | x | x | x
+      val board = Board(row0, BoardRow.empty, BoardRow.empty)
       val sacrificedSlots = List((0, 0))
 
       When("resolving the sacrifice")
@@ -22,3 +23,15 @@ class SacrificeManagerTest extends AnyFeatureSpec with GivenWhenThen with Matche
 
       Then("the squirrel should be removed and the slot (0, 0) should be empty")
       updatedBoard(0)(0) shouldBe x
+
+    Scenario("Sacrificing a card with ManyLives seal leaves it on the board"):
+      Given("A board with a cat(ManyLives) at row 1, column 2")
+      val row1 = x | x | Some(cat) | x
+      val board = Board(BoardRow.empty, row1, BoardRow.empty)
+      val sacrificedSlots = List((1, 2))
+
+      When("resolving the sacrifices")
+      val updatedBoard = SacrificeManager.resolveSacrifices(board, sacrificedSlots)
+
+      Then("the cat should not be removed and the slots (1,2) should still contain the cat")
+      updatedBoard(1)(2) shouldBe Some(cat)
