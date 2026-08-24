e1ee54e84536179acc4836a558d5c8c272e55775
PatriciaSanchezLomas <patricia.sanchez4@studio.unibo.it>
2026-08-24 14:13:04 +0000

    Update FireCampHealthView.scala

--- src/main/scala/org/scryption/view/events/FireCampHealthView.scala
+++ src/main/scala/org/scryption/view/events/FireCampHealthView.scala
@@ -4,11 +4,10 @@ import org.scryption.GameMessagesChannel
 import org.scryption.view.ViewModelDeckEvent
 import org.scryption.view.common.StatBonus
 
-/** Fire camp event: pick a card, boost its health by 2. */
-class FireCampHealthView(viewModel: ViewModelDeckEvent)
-    extends EventView(
-      viewModel = viewModel,
-      cardWidth = 250,
-      bonus = StatBonus.Health(2),
-      slotBgImagePath = "statboost_health"
-    )
+class FireCampHealthView(viewModel : ViewModelDeckEvent)
+  extends FireCampView(
+    viewModel = viewModel,
+    cardWidth = 250,
+    bonus = StatBonus.Health(2),
+    slotBgImagePath = "statboost_health"
+  )
