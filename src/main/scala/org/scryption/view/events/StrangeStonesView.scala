7e4eb2a3a93fab511a4f69b03a8fb5d1f6a40ca8
PatriciaSanchezLomas <patricia.sanchez4@studio.unibo.it>
2026-08-24 14:15:14 +0000

    Update StrangeStonesView.scala

--- src/main/scala/org/scryption/view/events/StrangeStonesView.scala
+++ src/main/scala/org/scryption/view/events/StrangeStonesView.scala
@@ -7,10 +7,11 @@ import java.awt.event.{MouseEvent, MouseListener}
 import java.awt.{Color, Cursor, Dimension, Graphics2D}
 import javax.swing.{ImageIcon, JLabel, SwingUtilities}
 
-import scala.concurrent.Future
 import scala.swing.{FlowPanel, Panel, Swing}
 
 class StrangeStonesView(viewModel: ViewModelDeckEvent) extends FlowPanel {
+
+  private var selectionState: Int = 0
 
   viewModel.ListenForCardsFromTheModel(renderHand)
 
@@ -40,7 +41,11 @@ class StrangeStonesView(viewModel: ViewModelDeckEvent) extends FlowPanel {
   private var upgradeIndex: Int = -1
   private var confirmLabel: Option[JLabel] = None
 
+  private var lockedSacrificeInfo: Option[CardViewInfo] = None
+  private var lockedSacrificeOriginalIndex: Int = -1
+
   opaque = false
+
 
   override protected def paintComponent(g: Graphics2D): Unit = {
     super.paintComponent(g)
@@ -57,20 +62,22 @@ class StrangeStonesView(viewModel: ViewModelDeckEvent) extends FlowPanel {
   }
 
   private def drawSlotOverlay(
-      g: Graphics2D,
-      overlay: Option[ImageIcon],
-      x: Int,
-      y: Int,
-      w: Int,
-      h: Int,
-      fallbackColor: Color
-  ): Unit =
+                               g: Graphics2D,
+                               overlay: Option[ImageIcon],
+                               x: Int,
+                               y: Int,
+                               w: Int,
+                               h: Int,
+                               fallbackColor: Color
+                             ): Unit =
     overlay match {
       case Some(icon) => g.drawImage(icon.getImage, x, y, w, h, peer)
       case None =>
         g.setColor(fallbackColor)
         g.fillRoundRect(x, y, w, h, 20, 20)
     }
+
+
 
   private sealed trait SlotKind
   private object SlotKind {
@@ -129,15 +136,17 @@ class StrangeStonesView(viewModel: ViewModelDeckEvent) extends FlowPanel {
 
       kind match {
         case SlotKind.Hand      => moveToFirstOpenSlot()
-        case SlotKind.Sacrifice => returnToHand()
+        case SlotKind.Sacrifice =>
         case SlotKind.Upgrade   => returnToHand()
       }
     }
 
     private def moveToFirstOpenSlot(): Unit = {
-      if (sacrificeIndex == -1) moveTo(SlotKind.Sacrifice)
-      else if (upgradeIndex == -1) moveTo(SlotKind.Upgrade)
-      // both full: clicking a hand card does nothing until a slot is freed
+      if (selectionState == 0) {
+        if (sacrificeIndex == -1) moveTo(SlotKind.Sacrifice)
+      } else if (selectionState == 1) {
+        if (upgradeIndex == -1) moveTo(SlotKind.Upgrade)
+      }
     }
 
     private def moveTo(target: SlotKind): Unit = {
@@ -155,21 +164,33 @@ class StrangeStonesView(viewModel: ViewModelDeckEvent) extends FlowPanel {
       currentY = slotY
       label.setLocation(currentX, currentY)
 
-      if (target == SlotKind.Sacrifice) sacrificeIndex = index else upgradeIndex = index
+      if (target == SlotKind.Sacrifice) {
+        sacrificeIndex = index
+        selectionState = 1
+        lockedSacrificeInfo = Some(info)
+        lockedSacrificeOriginalIndex = index
+        viewModel.sendCardToModel(index)
+        viewModel.ListenForCardsFromTheModel(renderHand)
+      } else if (target == SlotKind.Upgrade) {
+        upgradeIndex = index
+        refreshConfirmVisibility()
+      }
+
       refreshZOrder()
-      refreshConfirmVisibility()
     }
 
     private[StrangeStonesView] def returnToHand(): Unit = {
       if (isAnimating) return
-      if (kind == SlotKind.Sacrifice) sacrificeIndex = -1
-      if (kind == SlotKind.Upgrade) upgradeIndex = -1
-      kind = SlotKind.Hand
-      currentX = baseX
-      currentY = baseY
-      label.setLocation(currentX, currentY)
-      refreshZOrder()
-      refreshConfirmVisibility()
+
+      if (kind == SlotKind.Upgrade) {
+        upgradeIndex = -1
+        kind = SlotKind.Hand
+        currentX = baseX
+        currentY = baseY
+        label.setLocation(currentX, currentY)
+        refreshZOrder()
+        refreshConfirmVisibility()
+      }
     }
 
     private[StrangeStonesView] def moveToSlotCoords(target: SlotKind): Unit = {
@@ -200,37 +221,47 @@ class StrangeStonesView(viewModel: ViewModelDeckEvent) extends FlowPanel {
   private def confirmSacrifice(): Unit = {
     if (sacrificeIndex == -1 || upgradeIndex == -1) return
 
-    (cardSlots.find(_.index == sacrificeIndex), cardSlots.find(_.index == upgradeIndex)) match {
-      case (Some(sacrifice), Some(upgrade)) =>
-        sacrifice.isAnimating = true
+    (cardSlots.find(_.index == upgradeIndex)) match {
+      case Some(upgrade) =>
+
         upgrade.isAnimating = true
 
+        val sacrificeInfo = lockedSacrificeInfo.getOrElse(upgrade.info)
+
         val mergedInfo = upgrade.info.copy(
-          addedSigils = upgrade.info.addedSigils ++ sacrifice.info.defaultSigils ++ sacrifice.info.addedSigils
+          addedSigils = upgrade.info.addedSigils ++ sacrificeInfo.defaultSigils ++ sacrificeInfo.addedSigils
         )
         upgrade.applyMergedIcon(mergedInfo)
-        sacrifice.hide()
+
+        val sacrificeSlot = cardSlots.find(_.index == sacrificeIndex)
+        sacrificeSlot.foreach(_.hide())
+
         confirmLabel.foreach(_.setVisible(false))
 
         SwingUtilities.invokeLater(() => {
           Thread.sleep(800)
-          Swing.onEDT { sendResultToGameModel(sacrifice.index, upgrade.index) }
+          Swing.onEDT {
+            viewModel.sendCardToModel(upgrade.index)
+            selectionState = 0
+            sacrificeIndex = -1
+            upgradeIndex = -1
+            lockedSacrificeInfo = None
+          }
         })
       case _ => ()
     }
   }
 
-  private def sendResultToGameModel(sacrificeCardIndex: Int, upgradeCardIndex: Int): Unit = {
-    viewModel.sendCardToModel(sacrificeCardIndex)
-    viewModel.sendCardToModel(upgradeCardIndex)
-  }
-
   private def renderHand(cardsViewInfo: List[CardViewInfo]): Unit = {
+
+    val isUpgradingPhase = selectionState == 1
+
     contents.clear()
+
     cardSlots = Vector.empty
     confirmLabel = None
 
-    if (cardsViewInfo.isEmpty) return
+    if (cardsViewInfo.isEmpty && !isUpgradingPhase) return
 
     val handWidth = (cardsViewInfo.length - 1) * cardGap + geometry.cardWidth
     val pairWidth = geometry.cardWidth * 2 + slotGap
@@ -251,9 +282,32 @@ class StrangeStonesView(viewModel: ViewModelDeckEvent) extends FlowPanel {
 
     cardSlots = slots
 
-    if (sacrificeIndex != -1 && sacrificeIndex < slots.length)
-      slots(sacrificeIndex).moveToSlotCoords(SlotKind.Sacrifice)
-    if (upgradeIndex != -1 && upgradeIndex < slots.length) slots(upgradeIndex).moveToSlotCoords(SlotKind.Upgrade)
+    if (isUpgradingPhase && lockedSacrificeInfo.isDefined) {
+
+      val sacrificeInfo = lockedSacrificeInfo.get
+      val sacrificeSlot = new CardSlot(lockedSacrificeOriginalIndex, sacrificeInfo, -1, -1) {
+        override private[StrangeStonesView] def returnToHand(): Unit = {}
+      }
+
+      val slotW = geometry.cardWidth
+      val pairWidth = slotW * 2 + slotGap
+      val sacrificeX = (panel.preferredSize.width - pairWidth) / 2
+
+      sacrificeSlot.kind = SlotKind.Sacrifice
+      sacrificeSlot.currentX = sacrificeX
+      sacrificeSlot.currentY = slotY
+      sacrificeSlot.label.setLocation(sacrificeX, slotY)
+      sacrificeSlot.isAnimating = false
+
+      panel.peer.add(sacrificeSlot.label)
+
+      cardSlots = cardSlots :+ sacrificeSlot
+      sacrificeIndex = lockedSacrificeOriginalIndex
+    }
+
+    if (upgradeIndex != -1 && upgradeIndex < slots.length) {
+      slots(upgradeIndex).moveToSlotCoords(SlotKind.Upgrade)
+    }
 
     val confirm: JLabel = new JLabel(confirmIcon.orNull) {
       setBounds(
