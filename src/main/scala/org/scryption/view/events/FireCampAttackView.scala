c605787c44b60c04fdefcb3a81c1ca70ffbba5d7
PatriciaSanchezLomas <patricia.sanchez4@studio.unibo.it>
2026-08-24 14:13:24 +0000

    Update FireCampAttackView.scala

--- src/main/scala/org/scryption/view/events/FireCampAttackView.scala
+++ src/main/scala/org/scryption/view/events/FireCampAttackView.scala
@@ -4,10 +4,10 @@ import org.scryption.GameMessagesChannel
 import org.scryption.view.ViewModelDeckEvent
 import org.scryption.view.common.StatBonus
 
-class FireCampAttackView(viewModel: ViewModelDeckEvent)
-    extends EventView(
-      viewModel = viewModel,
-      cardWidth = 250,
-      bonus = StatBonus.Attack(1),
-      slotBgImagePath = "statboost_attack"
-    )
+class FireCampAttackView(viewModel : ViewModelDeckEvent)
+  extends FireCampView(
+    viewModel = viewModel,
+    cardWidth = 250,
+    bonus = StatBonus.Attack(1),
+    slotBgImagePath = "statboost_attack"
+  )
